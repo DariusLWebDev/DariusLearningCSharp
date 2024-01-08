c7dbac92dc5cf29fbfac3ced6a45acc8a7e5889f
DariusLWebDev <143473187+DariusLWebDev@users.noreply.github.com>
2024-01-08 13:26:46 +0800

    Learning Loops

--- DariusLearningCSharp/DariusLearningCSharp/Program.cs
+++ DariusLearningCSharp/DariusLearningCSharp/Program.cs
@@ -103,13 +103,126 @@ namespace DariusLearningCSharp
             Console.WriteLine(Convert.ToInt32(myDouble2));
             Console.WriteLine(Convert.ToString(myBool2));
 
+            // User Input
+            Console.WriteLine("Enter username:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Username is: " + userName);
+
+            //Comparison
+
+            int x2 = 5;
+            int y2 = 3;
+            Console.WriteLine(x2 < y2);
+
+            Console.WriteLine(x2 > 1 && x2 < y2);
+
+            string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            Console.WriteLine("The lenght of the txt string is: " + txt.Length);
 
 
+            Console.WriteLine(txt.ToUpper());
+            Console.WriteLine(txt.ToLower());
+
+            string firstName3 = "John";
+            string lastName = "Doe";
+            string name2 = $"My full name is: {firstName3} {lastName}";
+            Console.WriteLine(name2);
+
+            //if...Else
+            if (20 > 18)
+            {
+                Console.WriteLine("20 is greater than 18");
+            }
+
+            int time = 20;
+            if (time < 18)
+            {
+                Console.WriteLine("Good Day");
+            }
+            else
+            {
+                Console.WriteLine("Good Evening");
+            }
+
+            int time2 = 22;
+            if (time2 < 10)
+            {
+                Console.WriteLine("Good Day");
+            }
+            else if (time2 < 20) {
+                Console.WriteLine("Good Morning");
+            }
+            else
+            {
+                Console.WriteLine("Good Evening");
+            }
+
+            //Shorthand
+
+            int time3 = 20;
+            string result = (time3 < 18) ? "Good day." : "Good evening";
+            Console.WriteLine(result);
+
+            //Switch
+
+            int day = 9;
+            switch (day)
+            {
+                case 1:
+                Console.WriteLine("Monday");
+                break;
+                case 2:
+                Console.WriteLine("Tuesday"); 
+                break;
+                case 3:
+                Console.WriteLine("Wednesday");
+                break;  
+                case 4:
+                Console.WriteLine("Thursday");
+                break;
+                case 5:
+                Console.WriteLine("Friday");
+                break;
+                case 6:
+                Console.WriteLine("Saturday");
+                break;
+                case 7:
+                Console.WriteLine("Sunday");
+                break;
+                default: 
+                Console.WriteLine("Looking forward to the Weekend");
+                break;
+            }
+
+            //loops
+
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            int v = 0;
+            do
+            {
+                Console.WriteLine(v);
+                v++;
+            }
+            while (v < 10);
 
 
+            for (int z = 0; z < 5; z++)
+            {
+                Console.WriteLine(z);
+            }
 
-
-
+            for (int g = 0; g <= 5; g = g + 1)
+            {
+                Console.WriteLine(g);
+            }
+            
         }
     }
 }
